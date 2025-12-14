d9eafb22fcf3885c5a3a4f05a41028fc6069eb7f
Nicojrz <juarez.barrios.axel.nicolas@gmail.com>
2025-12-14 17:58:16 -0600

    init

---
+++ src/java/com/universal/app/model/Connector.java
@@ -0,0 +1,39 @@
+package com.universal.app.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connector
+{
+    private String username = "root";
+    private String password = "admin";
+    private String hostname = "localhost";
+    private String port = "3306";
+    private String db = "universal";
+    
+    private String classname = "com.mysql.cj.jdbc.Driver";
+    
+    private String url = "jdbc:mysql://" + hostname + ":" + port + "/" + db + "?useUnicode=true&characterEncoding=UTF-8";
+    
+    private Connection connection;
+    
+    public Connector()
+    {
+        try
+        {
+            Class.forName(classname);
+            connection = DriverManager.getConnection(url,username,password);
+        }
+        catch (ClassNotFoundException | SQLException e)
+        {
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    public Connection getConnection()
+    {
+        return connection;
+    }
+}
+
